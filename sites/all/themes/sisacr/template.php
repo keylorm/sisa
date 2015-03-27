0ffd2e725cd8f7104cca8f0739d2cc10e5c44426
Keylor Mora <keylor@orbelink.com>
2015-03-27 10:27:03 -0600

    cambio en li de menu

--- sites/all/themes/sisacr/template.php
+++ sites/all/themes/sisacr/template.php
@@ -127,3 +127,12 @@ function sisacr_preprocess_block(&$vars, $hook) {
   // Add a striping class.
   $vars['classes_array'][] = 'block-' . $vars['zebra'];
 }
+
+function sisacr_menu_link(array $variables) {
+//add class for li
+   $variables['element']['#attributes']['class'][] = 'menu-' . $variables['element']['#original_link']['mlid'];
+//add class for a
+   $variables['element']['#localized_options']['attributes']['class'][] = 'menu-' . $variables['element']['#original_link']['mlid'];
+//dvm($variables['element']);
+  return theme_menu_link($variables);
+}
